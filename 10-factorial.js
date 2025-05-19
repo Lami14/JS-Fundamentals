c1c7c744ba4b78e1ceb8a4fc191a075ee35cb57b
Lami14 <123112707+Lami14@users.noreply.github.com>
2025-05-19 20:46:25 +0200

    Update 10-factorial.js

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,10 +1,27 @@
 #!/usr/bin/node
 
-function add(a, b) {
-  return a + b;
+function factorial(n) {
+  if (n === 0n || n === 1n) {
+    return 1n;
+  }
+  let result = 1n;
+  for (let i = 2n; i <= n; i++) {
+    result *= i;
+  }
+  return result;
 }
 
-const arg1 = parseInt(process.argv[2]);
-const arg2 = parseInt(process.argv[3]);
+const arg = process.argv[2];
+if (!arg) {
+  console.log("1");
+  process.exit(0);
+}
 
-console.log(add(arg1, arg2));
+const num = BigInt(arg);
+
+if (num < 0n) {
+  console.log("1"); // factorial for negative is undefined, print 1 or handle differently
+  process.exit(0);
+}
+
+console.log(factorial(num).toString());
