459acead27b778fc2b34158fdff347a751e4a148
Lami14 <123112707+Lami14@users.noreply.github.com>
2025-05-19 20:34:25 +0200

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -3,10 +3,9 @@
 const arg = process.argv[2];
 const num = parseInt(arg);
 
-if (isNaN(num) || num <= 0) {
-  console.log("Missing number of occurrences");
-} else {
+if (!isNaN(num) && num > 0) {
   for (let i = 0; i < num; i++) {
     console.log("C is fun");
   }
 }
+// no else block, so no output if invalid or missing
