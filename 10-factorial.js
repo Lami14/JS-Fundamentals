f402d9260675527f8af4cdd95153b1425d78b0ad
Lami14 <123112707+Lami14@users.noreply.github.com>
2025-05-26 02:49:07 +0200

    Update 10-factorial.js

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,42 +1,11 @@
-#!/usr/bin/node
-
 function factorial(n) {
-  if (n === 0n || n === 1n) {
-    return 1n;
+  if (isNaN(n) || n === 0) {
+    return 1;
   }
-  let result = 1n;
-  for (let i = 2n; i <= n; i++) {
-    result *= i;
-  }
-  return result;
+  return n * factorial(n - 1);
 }
 
 const arg = process.argv[2];
-if (!arg) {
-  console.log("1");
-  process.exit(0);
-}
+const num = parseInt(arg, 10);
 
-let num;
-try {
-  num = BigInt(arg);
-} catch {
-  console.log("1");
-  process.exit(0);
-}
-
-if (num < 0n) {
-  console.log("1");
-  process.exit(0);
-}
-
-const fact = factorial(num);
-
-const factNum = Number(fact);
-if (factNum === Infinity) {
-  // Number too big to represent, print full BigInt string
-  console.log(fact.toString());
-} else {
-  // print scientific notation for numbers that fit in Number
-  console.log(factNum.toExponential());
-}
+console.log(factorial(isNaN(num) ? 1 : num));
