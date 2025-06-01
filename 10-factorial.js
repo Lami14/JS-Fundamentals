a427dccd0819f9ba5313c75e9da6e16d52de2f85
Lami14 <123112707+Lami14@users.noreply.github.com>
2025-06-01 20:13:26 +0200

    Update 10-factorial.js

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,11 +1,12 @@
 function factorial(n) {
-  if (isNaN(n) || n === 0) {
+  if (isNaN(n) || n === 0 || n === 1) {
     return 1;
+  } else if (n < 0) {
+    return 'Error: Factorial is not defined for negative numbers';
+  } else {
+    return n * factorial(n - 1);
   }
-  return n * factorial(n - 1);
 }
 
-const arg = process.argv[2];
-const num = parseInt(arg, 10);
-
-console.log(factorial(isNaN(num) ? 1 : num));
+const input = parseInt(process.argv[2]);
+console.log(factorial(input));
